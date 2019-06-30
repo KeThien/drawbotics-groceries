58a4ba6929d33c98d217f4732f7665cc06038591
Ke Thien <giakethien@gmail.com>
2019-06-30 23:13:38 +0200

    structure for moduling

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,6 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+    <TheHeading />
 
     <v-content>
       <HelloWorld/>
@@ -23,11 +10,13 @@
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import TheHeading from './components/core/TheHeading'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TheHeading
   },
   data () {
     return {
