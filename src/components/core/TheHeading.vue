9faf83d9cbf5018e4d6c364f5f36d5a0fa1fe60d
Ke Thien <giakethien@gmail.com>
2019-07-10 11:03:07 +0200

    Prepare the navbar header for login btn

--- src/components/core/TheHeading.vue
+++ src/components/core/TheHeading.vue
@@ -4,14 +4,28 @@
       <span>Groceries APP</span>
     </v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-btn flat>
+    <v-btn v-if="isLogged === false" flat>
       <span>Login</span>
+    </v-btn>
+    <v-btn v-if="isLogged === true" flat>
+      <span>{{user}}</span>
+    </v-btn>
+    <v-btn v-if="isAdmin === true && isLogged === true" flat>
+      <span>Admin Panel</span>
     </v-btn>
   </v-toolbar>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isLogged: true,
+      isAdmin: true,
+      user: "user"
+    };
+  }
+};
 </script>
 
 <style>
